38085267e36f2e5950533f1af7a3b2b2166d7bf8
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-18 11:20:49 +0100

    updated to use initial user list

--- src/script.ts
+++ src/script.ts
@@ -1,29 +1,45 @@
 import {clearNode} from './lib/dom.js';
-import {button, h1, input, li, ul} from './lib/html.js';
+import {button, fieldset, h1, input, legend, li, span, ul} from './lib/html.js';
 import {NodeMap, node} from './lib/nodes.js';
-import {inited} from './rpc.js';
+import {inited, rpc} from './rpc.js';
 
-inited.then(rpc => {
-	const name = input();
-	clearNode(document.body, [
+type userNode = {
+	[node]: HTMLLIElement;
+}
+
+inited.then(userList => {
+	let connected = false;
+	const users = new NodeMap<string, userNode>(ul()),
+	      name = input(),
+	      addName = (name: string) => users.set(name, {
+		[node]: li({"onclick": () => { // local
+			if (!connected) {
+				return;
+			}
+		}}, name)
+	      }),
+	      error = span(),
+	      fs = fieldset([
+		legend("Enter Name"),
 		name,
-		button({"onclick": () => rpc.init(name.value).then(list => {
-			type nameNode = {
-				[node]: HTMLLIElement;
-			}
-			const nameList = new NodeMap<string, nameNode>(ul()),
-			      addName = (name: string) => nameList.set(name, {
-				[node]: li(name)
-			      });
-			for (const name of list) {
-				addName(name);
-			}
-			clearNode(document.body, [
-				h1(name.value),
-				nameList[node]
-			]);
-			rpc.waitUserAdd().then(addName);
-			rpc.waitUserRemove().then(name => nameList.delete(name));
-		})}, "Connect")
+		button({"onclick": () => {
+			const n = name.value;
+			rpc.init(n)
+			.then(() => {
+				fs.replaceWith(h1(n))
+				connected = true;
+			})
+			.catch(e => clearNode(error, e + ""));
+		}}, "Connect"),
+		error
+	      ]);
+	for (const user of userList) {
+		addName(user);
+	}
+	clearNode(document.body, [
+		fs,
+		users[node]
 	]);
+	rpc.waitUserAdd().then(addName);
+	rpc.waitUserRemove().then(name => users.delete(name));
 });
